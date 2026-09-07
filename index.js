2ba0180bf9a0805dfb79bfdf520e41efe718a734
aswe12300-eng <aswe12300@gmail.com>
2026-09-08 03:24:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const {
   setupSupportRole
 } = require("./src/supportRole");
 
+const {
+  setupBossTracker
+} = require("./src/bossTracker");
+
 setupRegistration(client);
 setupSupportRole(client);
+setupBossTracker(client);
 
 client.login(process.env.DISCORD_TOKEN).catch(error => {
   console.error("Discord 登入失敗：", error);
