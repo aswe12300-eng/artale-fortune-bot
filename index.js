2ecdf74fb1ac1a09d87b34db8f5be1e49beeebfb
aswe12300-eng <aswe12300@gmail.com>
2026-09-07 22:49:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { client } = require("./src/coreBot");
-const { setupRegistration } = require("./src/registration");
+const {
+  setupRegistration
+} = require("./src/registration");
+
+const {
+  setupSupportRole
+} = require("./src/supportRole");
 
 setupRegistration(client);
+setupSupportRole(client);
 
 client.login(process.env.DISCORD_TOKEN).catch(error => {
   console.error("Discord 登入失敗：", error);
